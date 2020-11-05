1dc842b2256e141974769dd4983a76a26a2e6d68
rtnkanth <rtnkanth@gmail.com>
2020-11-05 23:34:13 +0530

    Joke App

---
+++ src/main/java/rtnk/springframework/joke/services/JokeService.java
@@ -0,0 +1,6 @@
+package rtnk.springframework.joke.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/rtnk/springframework/joke/services/JokeServiceImpl.java
@@ -0,0 +1,17 @@
+package rtnk.springframework.joke.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
